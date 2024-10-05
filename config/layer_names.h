b770cbd31cc399286f659c09da0b59a67f0dec10
Emil Kantis <emil@codeboss.se>
2024-10-05 12:47:48 +0200

    Shortening some layer names

--- config/layer_names.h
+++ config/layer_names.h
@@ -2,10 +2,10 @@
 #define BASE 0
 #define LOWER 1 
 #define MAGIC 2
-#define RH_NUMPAD 3
-#define RH_FN_KEYS 4
+#define NUM 3
+#define FN 4
 #define SWE 5
-#define LH_PROG 7
+#define PROG 7
 #define CURSOR 8
 #define SYMBOLS 9
 
