1e35e3bd403729c3ba3694fc6aeafd3ee4abb736
Emil Kantis <emil@codeboss.se>
2024-10-05 12:24:20 +0200

    Moving macros to separate file

---
+++ config/macros.h
@@ -0,0 +1,129 @@
+#include "zmk-nodefree-config/include/zmk-helpers/helper.h"
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Approximation of Pascal Getreuer's Select Word macro from QMK
+// - https://getreuer.info/posts/keyboards/select-word/index.html
+//
+//////////////////////////////////////////////////////////////////////////
+
+// SELECT_WORD_DELAY defines how long the macro waits (milliseconds)
+// after moving the cursor before it selects a word.  A larger delay
+// may allow the macro to move to the next word upon each invocation.
+#define SELECT_WORD_DELAY 1
+
+ZMK_MACRO(select_none,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp DOWN &kp UP &kp RIGHT &kp LEFT>;
+)
+
+
+/**
+ * Select word, including mod-morph for selecting left by holding shift
+ */
+ZMK_MOD_MORPH(select_word,
+    bindings = <&select_word_right>, <&select_word_left>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+ZMK_MACRO(select_word_right,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp LA(RIGHT) &kp LA(LEFT) &kp LA(LS(RIGHT))>;
+)
+
+ZMK_MACRO(select_word_left,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp LA(LEFT) &kp LA(RIGHT) &kp LA(LS(LEFT))>;
+)
+
+
+/**
+ * Extend word
+ */
+ZMK_MOD_MORPH(extend_word,
+    bindings = <&extend_word_right>, <&extend_word_left>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+ZMK_MACRO(extend_word_right,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp LA(LS(RIGHT))>;
+)
+
+ZMK_MACRO(extend_word_left,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp LA(LS(LEFT))>;
+)
+
+
+/**
+ * Select line
+ */
+ZMK_MOD_MORPH(select_line,
+    bindings = <&select_line_right>, <&select_line_left>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+ZMK_MACRO(select_line_right,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp HOME &kp LS(END)>;
+)
+
+ZMK_MACRO(select_line_left,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp END &kp LS(HOME)>;
+)
+
+
+/**
+ * Extend line
+ * - Extends current selection by one line
+ */
+ZMK_MOD_MORPH(extend_line,
+    bindings = <&extend_line_right>, <&extend_line_left>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+ZMK_MACRO(extend_line_right,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp LS(DOWN) &kp LS(END)>;
+)
+
+ZMK_MACRO(extend_line_left,
+    wait-ms = <SELECT_WORD_DELAY>;
+    tap-ms = <SELECT_WORD_DELAY>;
+    bindings = <&kp LS(UP) &kp LS(HOME)>;
+)
+
+
+/**
+ * Swedish special characters
+ */
+ZMK_MACRO(SWE_aring,
+    bindings = <&macro_press &kp LALT>, <&macro_tap &kp W>, <&macro_release &kp LALT>;
+)
+
+ZMK_MACRO(SWE_aumlaut,
+    bindings = <&macro_press &kp LALT>, <&macro_tap &kp A>, <&macro_release &kp LALT>;
+)
+
+ZMK_MACRO(SWE_oumlaut,
+    bindings = <&macro_press &kp LALT>, <&macro_tap &kp O>, <&macro_release &kp LALT>;
+)
+
+/**
+ * Programming macros for special symbol combinations
+ */
+
+// -> 
+ZMK_MACRO(PROG_arrow,
+    bindings = <&macro_tap &kp MINUS &kp GREATER_THAN>;
+)
