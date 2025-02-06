8dccb09e9e37230990b65a7e33f9489cd495cb41
Emil Kantis <emil@codeboss.se>
2025-02-06 07:54:36 +0100

    only allow combos from base layer

--- config/combos.h
+++ config/combos.h
@@ -1,8 +1,10 @@
 #include "zmk-nodefree-config/include/zmk-helpers/helper.h"
 #include "zmk-nodefree-config/include/zmk-helpers/key-labels/glove80.h"
 
+#include "layer_names.h"
+
 #define COMBO_TERM_FAST 50
 
-ZMK_COMBO(arrow, &PROG_arrow, RB2 RB3, ALL, COMBO_TERM_FAST)
-ZMK_COMBO(elvis, &PROG_elvis, RB1 RB2, ALL, COMBO_TERM_FAST)
+ZMK_COMBO(arrow, &PROG_arrow, RB2 RB3, BASE, COMBO_TERM_FAST)
+ZMK_COMBO(elvis, &PROG_elvis, RB1 RB2, BASE, COMBO_TERM_FAST)
 
